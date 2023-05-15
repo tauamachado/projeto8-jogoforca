bd6183128aa188b5405182b4924612cd15577c64
Tauã Machado <taua.machado@estudante.ufjf.br>
2023-05-15 17:12:28 -0300

    Tempo acabando, versão final!

--- src/components/Jogo.js
+++ src/components/Jogo.js
@@ -1,113 +1,82 @@
-import palavras from "./palavras"
-import forca0 from "../assets/forca0.png"
-import forca1 from "../assets/forca1.png"
-import forca2 from "../assets/forca2.png"
-import forca3 from "../assets/forca3.png"
-import forca4 from "../assets/forca4.png"
-import forca5 from "../assets/forca5.png"
-import forca6 from "../assets/forca6.png"
-import { useState, useEffect } from "react"
-import Letras from "./Letras"
+import palavras from "./palavras";
+import forca0 from "../assets/forca0.png";
+import forca1 from "../assets/forca1.png";
+import forca2 from "../assets/forca2.png";
+import forca3 from "../assets/forca3.png";
+import forca4 from "../assets/forca4.png";
+import forca5 from "../assets/forca5.png";
+import forca6 from "../assets/forca6.png";
+import { useState } from "react";
+import Letras from "./Letras";
 
-export default function Jogo({}) {
+const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6];
 
-  const [palavraEscolhida, setPalavraEscolhida] = useState([])
-  const [erros, setErros] = useState(0)
-  const [ativarLetras, setAtivarLetras] = useState(true)
-  const [ganhou, setGanhou] = useState(false)
-  const [perdeu, setPerdeu] = useState(false)
+const underline = () => <span className="editar-underline">_</span>;
+
+export default function Jogo() {
+  const [palavraEscolhida, setPalavraEscolhida] = useState([]);
+  const [erros, setErros] = useState(0);
+  const [ativarLetras, setAtivarLetras] = useState(true);
+  const [ganhou, setGanhou] = useState(false);
+  const [perdeu, setPerdeu] = useState(false);
   const [letrasEscolhidas, setLetrasEscolhidas] = useState([]);
-
-  
-  const images = [forca0, forca1, forca2, forca3, forca4, forca5, forca6]
+  const [jogarNovamente, setJogarNovamente] = useState(false);
 
   const palavraAleatoria = () => {
-
-    const aleatoria = Math.floor(Math.random() * palavras.length)
-
-    return palavras[aleatoria]
+    const aleatoria = Math.floor(Math.random() * palavras.length);
+    return palavras[aleatoria];
   };
 
   const comecarJogo = () => {
-
-    const pegarAleatoria = palavraAleatoria()
- 
+    const pegarAleatoria = palavraAleatoria();
     setPalavraEscolhida(
       pegarAleatoria.split("").map((letra) => ({ valor: letra, acertou: false }))
-    )
-
-    setErros(0)
-    setAtivarLetras(false)
-    setGanhou(false)
-    setPerdeu(false)
-
-  }
-    
-  const underline = () => {
-    return <span className="editar-underline">_</span>
-  }
-
-  useEffect(() => {
-
-    if (!palavraEscolhida.some((letra) => !letra.acertou)) {
-
-      setGanhou(true)
-      setAtivarLetras(true)
-
-    }
-  }, [palavraEscolhida, erros])
-
-
-  useEffect(() => {
-
-    if (erros + 1 === images.length) {
-
-      setPerdeu(true)
-      setAtivarLetras(true)
-
-    }
-  }, [erros])
+    );
+    setErros(0);
+    setAtivarLetras(false);
+    setGanhou(false);
+    setPerdeu(false);
+    setLetrasEscolhidas([]);
+    setJogarNovamente(false);
+  };
 
   const verificarLetra = (letra) => {
-
-    let acertou = false
-
+    let acertou = false;
     const novaPalavra = palavraEscolhida.map((l) => {
-
-      if (l.valor === letra) {
-
-        acertou = true
-        setAtivarLetras(false)
-        console.log("acertou")
-
-        return { ...l, acertou: true }
-
+      if (l.valor === letra.toLowerCase()) {
+        acertou = true;
+        setAtivarLetras(false);
+        console.log("acertou");
+        return { ...l, acertou: true };
       }
-      console.log("errou")
-      setAtivarLetras(false)
-
-      return l
-
-    })
-    setPalavraEscolhida(novaPalavra)
-
+      console.log("errou");
+      setAtivarLetras(false);
+      return l;
+    });
+    setPalavraEscolhida(novaPalavra);
     if (!acertou) {
-
-      setErros(erros + 1)
-
+      setErros(erros + 1);
     }
+    setLetrasEscolhidas([...letrasEscolhidas, letra.toLowerCase()]);
+    if (!novaPalavra.some((letra) => !letra.acertou)) {
+      setGanhou(true);
+      setAtivarLetras(true);
+      setJogarNovamente(true);
+    }
+    if (erros + 1 === images.length - 1) {
+      setPerdeu(true);
+      setAtivarLetras(true);
+      setJogarNovamente(true);
+    }
+  };
 
-setLetrasEscolhidas([...letrasEscolhidas, letra.toLowerCase()])
-
-  }
-  
   return (
     <div className="container-pai">
       <div key="container" className="container">
         <img data-test="game-image" className="" src={images[erros]} alt={images[erros]}></img>
         <div className="container-right">
-          <button data-test="choose-word" onClick={comecarJogo}>Escolher Palavra</button>
-          <div className={`palavra-escolhida ${ganhou ? 'acertou' : ''} ${perdeu ? 'errou' : ''}`}>
+        <button data-test="choose-word" onClick={comecarJogo}>Escolher Palavra</button>
+          <div data-test="word" className={`palavra-escolhida ${ganhou ? 'acertou' : ''} ${perdeu ? 'errou' : ''}`}>
             {palavraEscolhida.map((letra, index) => (
               <span data-test="word" key={index}>
                 {letra.acertou || perdeu ? letra.valor : underline("_")}
--- src/components/Letras.js
+++ src/components/Letras.js
@@ -1,15 +1,13 @@
 export default function Letras({ verificarLetra, ativarLetras, letrasEscolhidas}) {
     
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  
+    
     return (
       <div className="alfabeto">
         {alfabeto.map((letra) => (
-          <button data-test="letter" className={`botao ${ativarLetras || letrasEscolhidas.includes(letra.toLowerCase())? 'desabilitado' : 'habilitado'}` } disabled={ativarLetras ||letrasEscolhidas.includes(letra.toLowerCase())} key={letra} onClick={() => verificarLetra(letra)}>
-            {letra}
+          <button data-test="letter"key={letra} disabled={ativarLetras} onClick={() => verificarLetra(letra)}>            {letra.toUpperCase()}
           </button>
         ))}
       </div>
     );
   }
-  
